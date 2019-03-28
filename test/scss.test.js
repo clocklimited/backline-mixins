7e554a1da918065f059d5744019633dca95904df
Jack Brewer <jack@jackbrewer.co.uk>
2019-03-28 09:03:43 +0000

    Adds mixins with tests

---
+++ test/scss.test.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const sassTrue = require('sass-true')
+const glob = require('glob')
+
+describe('SCSS: Function', () => {
+  const sassTestFiles = glob.sync(
+    path.resolve(__dirname, 'function/**/*.test.scss')
+  )
+  sassTestFiles.forEach(file => sassTrue.runSass({ file }, describe, it))
+})
+
+describe('SCSS: Mixin', () => {
+  const sassTestFiles = glob.sync(
+    path.resolve(__dirname, 'mixin/**/*.test.scss')
+  )
+  sassTestFiles.forEach(file => sassTrue.runSass({ file }, describe, it))
+})
